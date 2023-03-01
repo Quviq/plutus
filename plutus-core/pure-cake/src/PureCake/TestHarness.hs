21e9d8abf9ab7f113790a9168d122c56a6da3b49
Maximilian Algehed <m.algehed@gmail.com>
2023-03-01 10:44:24 +0100

    Running test harness!

--- plutus-core/pure-cake/src/PureCake/TestHarness.hs
+++ plutus-core/pure-cake/src/PureCake/TestHarness.hs
@@ -1,12 +1,19 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ViewPatterns     #-}
 
 module PureCake.TestHarness where
 
+import Control.Lens
+import Control.Monad.Except
+import Control.Monad.Reader
 import Data.Text
 
 import PlutusCore.DeBruijn.Internal qualified as PLC
 import PlutusCore.Default.Builtins qualified as PLC
 import PlutusCore.Default.Universe qualified as PLC
 import PlutusCore.Evaluation.Machine.ExBudgetingDefaults qualified as PLC
+import PlutusCore.Quote
+import PlutusCore.TypeCheck qualified as PLC
 import UntypedPlutusCore.Core qualified as PLC
 import UntypedPlutusCore.Evaluation.Machine.Cek.EmitterMode qualified as PLC
 import UntypedPlutusCore.Evaluation.Machine.Cek.ExBudgetMode qualified as PLC
@@ -20,9 +27,17 @@ import PureCake.UntypedPlutusCore.Core qualified as Cake
 import PureCake.UntypedPlutusCore.Evaluation.Machine.Cek.ExBudgetMode qualified as Cake
 import PureCake.UntypedPlutusCore.Evaluation.Machine.Cek.Internal qualified as Cake
 
+
 import PureCake.ToPureCake
 
 import Test.QuickCheck
+
+import PlutusIR.Generators.QuickCheck.GenerateTerms
+import PlutusIR.Generators.QuickCheck.ShrinkTerms
+
+import PlutusCore.Compiler qualified as PLC
+import PlutusIR.Compiler
+import UntypedPlutusCore.DeBruijn qualified as PLC
 
 noEmitter :: Cake.EmitterMode uni fun
 noEmitter = Cake.EmitterMode $ \_ -> pure $ Cake.CekEmitterInfo (\_ -> pure ()) (pure mempty)
@@ -56,6 +71,16 @@ runCake =
                       noEmitter
   . termToCake
 
-prop_run_PLC_Cake :: PLC.Term PLC.NamedDeBruijn PLC.DefaultUni PLC.DefaultFun () -> Property
-prop_run_PLC_Cake tm =
-  runPLC tm === runCake tm
+prop_run_PLC_Cake :: Property
+prop_run_PLC_Cake =
+  forAllShrink genTypeAndTerm_ shrinkClosedTypedTerm $ \ (_, tm) ->
+    let etm' = runExcept $ do
+          tcConfig <- PLC.getDefTypeCheckConfig noProvenance
+          let pirCtx = toDefaultCompilationCtx tcConfig & set ccTypeCheckConfig Nothing
+          ctm <- flip runReaderT pirCtx $ runQuoteT $ compileTerm tm
+          ctm' <- flip runReaderT PLC.defaultCompilationOpts $ runQuoteT $ PLC.compileTerm ctm
+          ctm'' <- PLC.deBruijnTerm @(Error _ _ _) ctm'
+          pure $ () <$ ctm''
+    in case etm' of
+        Left _    -> error "Something wrong"
+        Right tm' -> runPLC tm' === runCake tm'
